30ba24464513fcbd597989b9992c9b3404df2d7b
Panca Satrio Nugroho <pnugroho@binaracademy.org>
2022-03-28 21:21:07 +0700

    Membuat component burger

---
+++ src/Components/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Bun from './Bun'
+import Patty from './Patty'
+import Lettuce from './Lettuce'
+import Cheese from './Cheese'
+import Tomato from './Tomato'
+
+const Burger = () => {
+
+    return (
+        <>
+            <Bun />
+            <Patty />
+            <Lettuce />
+            <Cheese />
+            <Tomato />
+            <Bun />
+        </>
+    )
+
+}
+
+export default Burger
